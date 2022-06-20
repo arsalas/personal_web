adf4e794153882cb23c5c477d441349c84eed9b3
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-06-20 19:07:08 +0200

    update

--- src/components/Modal.php
+++ src/components/Modal.php
@@ -8,9 +8,8 @@ class Modal
 			<div class="modal-background"></div>
 			<div class="modal-content">
 				<div class="box">
-					<?php
-					if ($isVideo) : ?>
-						<figure class="image is-16by9">
+					<?php if ($isVideo) : ?>
+						<figure class="image is-16by9"> 
 							<iframe class="has-ratio" width="640" height="360" src="https://www.youtube.com/embed/YE7VzlLtp-4" frameborder="0" allowfullscreen></iframe>
 						</figure>
 					<?php else : ?>
