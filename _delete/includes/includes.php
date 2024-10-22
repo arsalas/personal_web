8bcd2defe3bef504c112cd7bc88d0de7e8c7e594
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2024-10-22 23:40:56 +0200

    astro init

--- _delete/includes/includes.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../vendor/autoload.php';
-require_once("../src/config/globals.php");
-include_once("../src/controllers/MailController.php");
-include_once("../src/helpers/Image.php");
-include_once("../src/helpers/Data.php");
-include_once("../src/mail/Mail.php");
