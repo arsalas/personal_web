c2ec0a173508283c9dbfabb36f9db33c86292a59
Alberto Ramirez <a.ramir.sa@gmail.com>
2024-10-26 17:01:36 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'
+// import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http://localhost:4321/',
+	site: 'http://albertoramirezsalas.com/',
 	output: 'static',
 	// adapter: vercel(),
 });
